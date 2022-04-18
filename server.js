01a06c2ff96ac070718c6b0236d1be6e56ccfeb3
sarinya kongseng <59682762+zsarinya10z@users.noreply.github.com>
2022-04-18 23:11:35 +0700

    TEST

---
+++ server.js
@@ -0,0 +1,5 @@
+const express = require('express')
+const app = express()
+const PORT =process.env.PORT || 5000
+app.get('/',(req,res)=>res.send(`Hello World`))
+app.listen(PORT,() => console.log(`Server started on PORT ${PORT}`))
